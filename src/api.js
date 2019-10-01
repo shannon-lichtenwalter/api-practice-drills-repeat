3dd85884cf1dbdf49255f15715335fdf8ef6c1bd
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-30 19:35:47 -0600

    through number 6 finished

--- src/api.js
+++ src/api.js
@@ -1,9 +1,33 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+// const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
 
-const getItems = function () {
-  return Promise.resolve('A successful response!');
+// const getItems = function () {
+//   return Promise.resolve('A successful response!');
+// };
+
+// export default {
+//   getItems
+// };
+
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ShannonLich';
+
+const getItems = function (){
+  return fetch(`${BASE_URL}/items`);
 };
 
+
+const createItem = function (name) {
+  const newItem = JSON.stringify({name});
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: newItem,
+  });
+};
+
+
+
 export default {
-  getItems
+  getItems,
+  createItem,
 };
